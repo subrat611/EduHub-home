4d5db47ccec8c1ad4ebeb6857ff3253201abc514
Subrat kumar jena <subratj611@gmail.com>
2024-02-07 22:53:25 +0530

    minor changes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -14,17 +14,15 @@ const HeroSection = () => {
 
   return (
     <div className="h-screen w-full flex justify-center items-center">
-      <div className="h-[50vh] w-[50vw]">
-        <div className="flex flex-col justify-center items-center">
-          <p className="text-center text-5xl capitalize font-medium">
-            Empower Your Learning Journey <br /> at o.p jindal university
-          </p>
-          <p className="capitalize text-center my-5 text-xl text-gray-700">
-            Welcome to our Learning Management System <br />
-            your gateway to a seamless educational experience.
-          </p>
-          <Button onClick={navigateToFeaturesSection}>Explore features</Button>
-        </div>
+      <div className="h-[40vh] w-[50vw] flex flex-col items-center">
+        <p className="text-center text-5xl capitalize font-medium">
+          Empower Your Learning Journey <br /> at o.p jindal university
+        </p>
+        <p className="capitalize text-center my-5 text-xl text-gray-700">
+          Welcome to our Learning Management System <br />
+          your gateway to a seamless educational experience.
+        </p>
+        <Button onClick={navigateToFeaturesSection}>Explore features</Button>
       </div>
     </div>
   );
