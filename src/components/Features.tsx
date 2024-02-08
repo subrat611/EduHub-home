52f08fce08c687d7591cff9e0ac3f481fbd469d6
Subrat kumar jena <subratj611@gmail.com>
2024-02-08 12:29:44 +0530

    feat: feature section responsive

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,8 +1,8 @@
 const Features = () => {
   return (
-    <div className="flex flex-col mb-10">
+    <div className="flex flex-col mb-10 px-3 md:px-0">
       <h1
-        className="text-3xl text-center text-gray-600 font-medium"
+        className="text-xl text-center text-gray-600 font-medium md:text-3xl"
         id="feature-section-heading"
       >
         Elevate Your Learning Experience <br /> with Our Dynamic Features
@@ -10,7 +10,7 @@ const Features = () => {
       <div className="mt-10">
         <div className="shadow-sm border rounded-lg px-3 py-5 my-5">
           <div>
-            <p className="text-xl font-medium text-gray-800">
+            <p className="text-lg md:text-xl font-medium text-gray-800">
               Course Creation and Management
             </p>
             <ol className="px-5 list-disc space-y-1 mt-2">
@@ -27,7 +27,7 @@ const Features = () => {
 
         <div className="shadow-sm border rounded-lg px-3 py-5 my-5">
           <div>
-            <p className="text-xl font-medium text-gray-800">
+            <p className="text-lg md:text-xl font-medium text-gray-800">
               Effortless Exam Scheduling
             </p>
             <ol className="px-5 list-disc space-y-1 mt-2">
@@ -45,7 +45,7 @@ const Features = () => {
 
         <div className="shadow-sm border rounded-lg px-3 py-5 my-5">
           <div>
-            <p className="text-xl font-medium text-gray-800">
+            <p className="text-lg md:text-xl font-medium text-gray-800">
               Mobile Responsive Design
             </p>
             <ol className="list-none space-y-1 mt-2">
