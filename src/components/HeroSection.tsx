9cf4143531216108529d62c4e68810f8a565eaff
Subrat kumar jena <subratj611@gmail.com>
2024-02-08 12:27:29 +0530

    feat: hero section responsive

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -13,13 +13,13 @@ const HeroSection = () => {
 
   return (
     <div className="h-screen w-full flex justify-center items-center">
-      <div className="h-[40vh] w-[50vw] flex flex-col items-center">
-        <p className="text-center text-5xl capitalize font-medium">
+      <div className="h-[40vh] w-[90vw] md:w-[50vw] flex flex-col items-center">
+        <p className="text-2xl text-center md:text-5xl capitalize font-medium">
           Empower Your Learning Journey <br /> at o.p jindal university
         </p>
-        <p className="capitalize text-center my-5 text-xl text-gray-700">
-          Welcome to our Learning Management System <br />
-          your gateway to a seamless educational experience.
+        <p className="capitalize text-center my-5 text-lg md:text-xl text-gray-700">
+          Welcome to our Learning Management System. your gateway to a seamless
+          educational experience.
         </p>
         <Button
           onClick={navigateToFeaturesSection}
